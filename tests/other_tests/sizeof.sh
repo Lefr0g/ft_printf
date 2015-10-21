5ad68ef1e430b700be242bba6fa4a7ee8496d587
Lefr0g <amulin@student.42.fr>
2015-10-21 16:37:53 +0200

    Minor sync cleanup

--- tests/other_tests/sizeof.sh
+++ tests/other_tests/sizeof.sh
@@ -2,7 +2,8 @@
 
 echo "\n\033[033m>>> Compiling sizeof.c :\033[0m\n"
 make re -C ../../
-clang -Wall -Werror -Wextra -I ../../includes/ -L ../../ -lftprintf sizeof.c \
+clang -Wall -Werror -Wextra sizeof.c \
+	-I ../../includes/ -L ../../ -lftprintf \
 	-o sizeof.out
 echo "\n\033[033m>>> Running sizeof.out :\033[0m\n"
 ./sizeof.out

f301a0fda5e8af1fae6d9591ffd2aab7050b828c
Lefr0g <antoine.mulin@gmail.com>
2015-11-26 14:09:04 +0100

    Fixed echo to interpret escape char

--- tests/other_tests/unit.sh
+++ tests/other_tests/unit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "\n\033[033m>>> Compiling dumb.c :\033[0m\n"
+echo -e "\n\033[033m>>> Compiling dumb.c :\033[0m\n"
 make -C ../../
 make -C ../../libft/
 gcc -Wall -Werror -Wextra -I ../../includes -L ../../ -lftprintf -L ../../libft/ -lft unit.c -o unit.out
-echo "\n\033[033m>>> Running dumb.out :\033[0m\n"
+echo -e "\n\033[033m>>> Running dumb.out :\033[0m\n"
 ./unit.out

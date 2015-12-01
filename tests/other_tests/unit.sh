9d25e3abd4d8fd85f097efbd54169fc89ae31302
Lefr0g <amulin@student.42.fr>
2015-12-01 15:09:20 +0100

    unit.sh : Added OS detection to avoid gcc compilation errors on OSX

--- tests/other_tests/unit.sh
+++ tests/other_tests/unit.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-echo -e "\n\033[033m>>> Compiling dumb.c :\033[0m\n"
+# OS detection (Linux / OSX)
+#
+OS_NAME=`uname -s`
+if [ $OS_NAME = "Linux" ]; then
+	CC=gcc
+elif [ $OS_NAME = "Darwin" ]; then
+	CC=clang
+fi
+
+echo -e "\n\033[033m>>> Compiling unit.c :\033[0m\n"
 make -C ../../
 make -C ../../libft/
-gcc -Wall -Werror -Wextra -I ../../includes unit.c -L ../../libft -lft -L ../../ -lftprintf -o unit.out
-echo -e "\n\033[033m>>> Running dumb.out :\033[0m\n"
+$CC -Wall -Werror -Wextra -I ../../includes/ unit.c -L ../../libft -lft -L ../../ -lftprintf -o unit.out
+echo -e "\n\033[033m>>> Running unit.out :\033[0m\n"
 ./unit.out
